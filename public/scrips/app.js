ac45fcce992d7b5c07bd804e7ac1a39bb3d332d7
awmiri <talasaz1382@gmail.com>
2025-01-02 18:19:02 +0330

    done nav and submenu event and overly back ground

--- public/scrips/app.js
+++ public/scrips/app.js
@@ -5,6 +5,12 @@ const moonIcons = $.querySelectorAll('.moon_icon');
 const getThemContent = $.querySelector('.them_content');
 const shopeIcon = $.querySelector('#shope_card');
 const getShopeArea = $.querySelector('#shop_area');
+const subMenuBtn = $.querySelector('.submenu_btn');
+const getSubMenu = $.querySelector('.submenu_close');
+const getNavMenu = $.querySelector('.nav');
+const closeNavBtn = $.querySelector('.close_nav');
+const openNavBtn = $.querySelector('.nav_btn--open');
+const overlay = $.querySelector('.overlay_close');
 // local storage for dark mood event
 if (window.localStorage.getItem('them') === 'dark') {
     $.documentElement.classList.add('dark')
@@ -36,4 +42,29 @@ shopeIcon.addEventListener('click', function (event) {
         getShopeArea.classList.add('invisible')
         getShopeArea.classList.add('opacity-0')
     }
+})
+// navmenu open & close
+closeNavBtn.addEventListener('click', function () {
+    getNavMenu.classList.add('-right-64')
+    getNavMenu.classList.remove('right-0')
+    overlay.classList.add('overlay_close')
+    overlay.classList.remove('overlay')
+})
+overlay.addEventListener('click', function () {
+    getNavMenu.classList.add('-right-64')
+    getNavMenu.classList.remove('right-0')
+    overlay.classList.add('overlay_close')
+    overlay.classList.remove('overlay')
+})
+openNavBtn.addEventListener('click', function () {
+    getNavMenu.classList.remove('-right-64')
+    getNavMenu.classList.add('right-0')
+    overlay.classList.remove('overlay_close')
+    overlay.classList.add('overlay')
+})
+// submenu open & close
+subMenuBtn.addEventListener('click', function (event) {
+    subMenuBtn.classList.toggle('rotate-180')
+    event.currentTarget.parentElement.classList.toggle('text-orange-300')
+    getSubMenu.classList.toggle('submenu_open')
 })
